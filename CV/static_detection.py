3b6d32f687db1c1c15ea52b5d68dc384c21ed1a1
Aman Bhargava <amanb2000@gmail.com>
2020-03-26 00:12:57 -0400

    Add sample data set 1 and create static image recognizer template

---
+++ CV/static_detection.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('assets/haarcascade_frontalface_alt2.xml')
+font = cv2.FONT_HERSHEY_SIMPLEX
+line_height = 100
+fractional_font_size = 1/20
+
+im_name = 'ir_demo_2'
+
+img = cv2.imread(str('../media/samples/'+im_name+'.jpg'), 0) # flag can be 0, 1, -1 == grayscale, color, original
+print(img.shape) # if the images DNE, img will be None
+
+font_size = fractional_font_size * max(img.shape)
+
+### Detecting the Face in the Photo ###
+
+face_detect = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=10)
+
+for (x, y, w, h) in face_detect:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 100, 100), 2)
+
+    cv2.putText(img,'Cough: Mild',(x+w+2, y+line_height), font, 3,(255,255,255),5,cv2.LINE_AA)
+    cv2.putText(img,'Shortness of Breath: Moderate',(x+w+2, y+line_height*2), font, 3,(255,255,255),5,cv2.LINE_AA)
+    cv2.putText(img,'Temperature: 37.1 C',(x+w+2, y+line_height*3), font, 3,(255,255,255),5,cv2.LINE_AA)
+    cv2.putText(img,'Heart Rate: 72 BPM',(x+w+2, y+line_height*4), font, 3,(255,255,255),5,cv2.LINE_AA)
+    cv2.putText(img,'Risk Assessment: Moderate',(x+w+2, y+line_height*6), font, 3,(255,255,255),8,cv2.LINE_AA)
+
+    break
+
+### Showing the Images ###
+
+cv2.imshow('image', img)
+
+cv2.waitKey(0) # the argument is the number of milliseconds before auto-close. 0 makes it infinite.
+
+cv2.destroyAllWindows()
+
+cv2.imwrite('../media/results/'+im_name+'.png', img)
+
+cv2.destroyAllWindows()
