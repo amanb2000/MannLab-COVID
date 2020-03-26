97e0912f79e8d0c0f1595cbaaa39297d333c6e30
Aman Bhargava <amanb2000@gmail.com>
2020-03-26 17:09:50 -0400

    Add 'pre-digital' values for risk assessments

--- CV/static_detection.py
+++ CV/static_detection.py
@@ -6,7 +6,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 line_height = 100
 fractional_font_size = 1/20
 
-im_name = 'ir_demo_2'
+im_name = 'adam'
 
 img = cv2.imread(str('../media/samples/'+im_name+'.jpg'), 0) # flag can be 0, 1, -1 == grayscale, color, original
 print(img.shape) # if the images DNE, img will be None
@@ -20,11 +20,11 @@ face_detect = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=1
 for (x, y, w, h) in face_detect:
     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 100, 100), 2)
 
-    cv2.putText(img,'Cough: Mild',(x+w+2, y+line_height), font, 3,(255,255,255),5,cv2.LINE_AA)
-    cv2.putText(img,'Shortness of Breath: Moderate',(x+w+2, y+line_height*2), font, 3,(255,255,255),5,cv2.LINE_AA)
-    cv2.putText(img,'Temperature: 37.1 C',(x+w+2, y+line_height*3), font, 3,(255,255,255),5,cv2.LINE_AA)
-    cv2.putText(img,'Heart Rate: 72 BPM',(x+w+2, y+line_height*4), font, 3,(255,255,255),5,cv2.LINE_AA)
-    cv2.putText(img,'Risk Assessment: Moderate',(x+w+2, y+line_height*6), font, 3,(255,255,255),8,cv2.LINE_AA)
+    cv2.putText(img,'Cough: 12.4%, Low Risk',(x+w+2, y+line_height), font, 3,(255,255,255),5,cv2.LINE_AA)
+    cv2.putText(img,'Shortness of Breath: 30.2%, Medium Risk',(x+w+2, y+line_height*2), font, 3,(255,255,255),5,cv2.LINE_AA)
+    cv2.putText(img,'Temperature: 37.1 C, Low Risk',(x+w+2, y+line_height*3), font, 3,(255,255,255),5,cv2.LINE_AA)
+    cv2.putText(img,'Heart Rate: 72 BPM, Low Risk',(x+w+2, y+line_height*4), font, 3,(255,255,255),5,cv2.LINE_AA)
+    cv2.putText(img,'Risk Assessment: 10.2%, Low Risk',(x+w+2, y+line_height*6), font, 3,(255,255,255),8,cv2.LINE_AA)
 
     break
 
