1e04683daa2ccdfe2a21d13e9b2c84f38e4158fc
Aman Bhargava <amanb2000@gmail.com>
2020-03-27 19:04:21 -0400

    Add IR image collection 2, create proposal graphic

--- CV/static_detection.py
+++ CV/static_detection.py
@@ -6,38 +6,46 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 line_height = 100
 fractional_font_size = 1/20
 
-im_name = 'adam'
+im_name = 'No_IR_Flip'
+im2_name = 'IR_Composit_Flip'
 
 img = cv2.imread(str('../media/samples/'+im_name+'.jpg'), 0) # flag can be 0, 1, -1 == grayscale, color, original
+
+im2 = cv2.imread(str('../media/samples/'+im2_name+'.jpg'), 1)
+
 print(img.shape) # if the images DNE, img will be None
 
-font_size = fractional_font_size * max(img.shape)
+font_size = 1.5
 
 ### Detecting the Face in the Photo ###
 
 face_detect = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=10)
 
+cnt = 0
+
 for (x, y, w, h) in face_detect:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 100, 100), 2)
+    if cnt != 0:
+        cv2.rectangle(im2, (x, y), (x+w, y+h), (255, 255, 255), 2)
 
-    cv2.putText(img,'Cough: 12.4%, Low Risk',(x+w+2, y+line_height), font, 3,(255,255,255),5,cv2.LINE_AA)
-    cv2.putText(img,'Shortness of Breath: 30.2%, Medium Risk',(x+w+2, y+line_height*2), font, 3,(255,255,255),5,cv2.LINE_AA)
-    cv2.putText(img,'Temperature: 37.1 C, Low Risk',(x+w+2, y+line_height*3), font, 3,(255,255,255),5,cv2.LINE_AA)
-    cv2.putText(img,'Heart Rate: 72 BPM, Low Risk',(x+w+2, y+line_height*4), font, 3,(255,255,255),5,cv2.LINE_AA)
-    cv2.putText(img,'Distance: 2.2m, Medium Risk',(x+w+2, y+line_height*5), font, 3,(255,255,255),5,cv2.LINE_AA)
+        cv2.putText(im2,'Cough: 12.4%, Low Risk',(x+w+2, y+line_height), font, font_size,(255,255,255),3,cv2.LINE_AA)
+        cv2.putText(im2,'Shortness of Breath: 30.2%, Medium Risk',(x+w+2, y+line_height*2), font, font_size,(255,255,255),3,cv2.LINE_AA)
+        cv2.putText(im2,'Temperature: 37.1 C, Low Risk',(x+w+2, y+line_height*3), font, font_size,(255,255,255),3,cv2.LINE_AA)
+        cv2.putText(im2,'Heart Rate: 72 BPM, Low Risk',(x+w+2, y+line_height*4), font, font_size,(255,255,255),3,cv2.LINE_AA)
+        cv2.putText(im2,'Distance: 2.2m, Medium Risk',(x+w+2, y+line_height*5), font, font_size,(255,255,255),3,cv2.LINE_AA)
 
-    cv2.putText(img,'Risk Assessment: 15.2%, Low Risk',(x+w+2, y+line_height*7), font, 3,(255,255,255),8,cv2.LINE_AA)
+        cv2.putText(im2,'Risk Assessment: 15.2%, Low Risk',(x+w+2, y+line_height*7), font, font_size+1,(100,255,100),5,cv2.LINE_AA)
 
-    break
+    cnt += 1
+    
 
 ### Showing the Images ###
 
-cv2.imshow('image', img)
+cv2.imshow('image', im2)
 
 cv2.waitKey(0) # the argument is the number of milliseconds before auto-close. 0 makes it infinite.
 
 cv2.destroyAllWindows()
 
-cv2.imwrite('../media/results/'+im_name+'.png', img)
+cv2.imwrite('../media/results/'+im2_name+'.png', im2)
 
 cv2.destroyAllWindows()
