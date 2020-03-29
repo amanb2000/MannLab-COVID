e59641407073cd137b8c91385f8306efc0d5915d
Aman Bhargava <amanb2000@gmail.com>
2020-03-28 21:30:14 -0400

    Create shortened radar graph of heart rate with Mann's suggestions

--- CV/static_detection.py
+++ CV/static_detection.py
@@ -15,7 +15,7 @@ im2 = cv2.imread(str('../media/samples/'+im2_name+'.jpg'), 1)
 
 print(img.shape) # if the images DNE, img will be None
 
-font_size = 1.5
+font_size = 2.3
 
 ### Detecting the Face in the Photo ###
 
@@ -27,13 +27,13 @@ for (x, y, w, h) in face_detect:
     if cnt != 0:
         cv2.rectangle(im2, (x, y), (x+w, y+h), (255, 255, 255), 2)
 
-        cv2.putText(im2,'Cough: 12.4%, Low Risk',(x+w+2, y+line_height), font, font_size,(255,255,255),3,cv2.LINE_AA)
-        cv2.putText(im2,'Shortness of Breath: 30.2%, Medium Risk',(x+w+2, y+line_height*2), font, font_size,(255,255,255),3,cv2.LINE_AA)
+        cv2.putText(im2,'Cough: 22.4%, Medium Risk',(x+w+2, y+line_height), font, font_size,(255,255,255),3,cv2.LINE_AA)
+        cv2.putText(im2,'Respiration: 12.2%, Low Risk',(x+w+2, y+line_height*2), font, font_size,(255,255,255),3,cv2.LINE_AA)
         cv2.putText(im2,'Temperature: 37.1 C, Low Risk',(x+w+2, y+line_height*3), font, font_size,(255,255,255),3,cv2.LINE_AA)
         cv2.putText(im2,'Heart Rate: 72 BPM, Low Risk',(x+w+2, y+line_height*4), font, font_size,(255,255,255),3,cv2.LINE_AA)
         cv2.putText(im2,'Distance: 2.2m, Medium Risk',(x+w+2, y+line_height*5), font, font_size,(255,255,255),3,cv2.LINE_AA)
 
-        cv2.putText(im2,'Risk Assessment: 15.2%, Low Risk',(x+w+2, y+line_height*7), font, font_size+1,(100,255,100),5,cv2.LINE_AA)
+        cv2.putText(im2,'Risk Assessment: 15.2%, Low Risk',(x+w+2, y+line_height*7), font, font_size+.2,(100,255,100),5,cv2.LINE_AA)
 
     cnt += 1
     
