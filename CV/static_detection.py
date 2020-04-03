42c1beee00e7911f9c2554f95d4db7b68225ea6a
Aman Bhargava <amanb2000@gmail.com>
2020-04-02 21:44:03 -0400

    Update CV

--- CV/static_detection.py
+++ CV/static_detection.py
@@ -3,7 +3,7 @@ import numpy as np
 
 face_cascade = cv2.CascadeClassifier('assets/haarcascade_frontalface_alt2.xml')
 font = cv2.FONT_HERSHEY_SIMPLEX
-line_height = 100
+line_height = 130
 fractional_font_size = 1/20
 
 im_name = 'No_IR_Flip'
@@ -15,7 +15,7 @@ im2 = cv2.imread(str('../media/samples/'+im2_name+'.jpg'), 1)
 
 print(img.shape) # if the images DNE, img will be None
 
-font_size = 2.3
+font_size = 5
 
 ### Detecting the Face in the Photo ###
 
@@ -23,17 +23,21 @@ face_detect = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=1
 
 cnt = 0
 
+
+font_weight = 9
+
 for (x, y, w, h) in face_detect:
     if cnt != 0:
         cv2.rectangle(im2, (x, y), (x+w, y+h), (255, 255, 255), 2)
 
-        cv2.putText(im2,'Cough: 22.4%, Medium Risk',(x+w+2, y+line_height), font, font_size,(255,255,255),3,cv2.LINE_AA)
-        cv2.putText(im2,'Respiration: 12.2%, Low Risk',(x+w+2, y+line_height*2), font, font_size,(255,255,255),3,cv2.LINE_AA)
-        cv2.putText(im2,'Temperature: 37.1 C, Low Risk',(x+w+2, y+line_height*3), font, font_size,(255,255,255),3,cv2.LINE_AA)
-        cv2.putText(im2,'Heart Rate: 72 BPM, Low Risk',(x+w+2, y+line_height*4), font, font_size,(255,255,255),3,cv2.LINE_AA)
-        cv2.putText(im2,'Distance: 2.2m, Medium Risk',(x+w+2, y+line_height*5), font, font_size,(255,255,255),3,cv2.LINE_AA)
+        cv2.putText(im2,'C: 22.4%',(x+w+2, y+line_height), font, font_size,(255,255,255),font_weight,cv2.LINE_AA)
+        cv2.putText(im2,'R: 12.2%',(x+w+2, y+line_height*2), font, font_size,(255,255,255),font_weight,cv2.LINE_AA)
+        cv2.putText(im2,'TMP: 37.1 C',(x+w+2, y+line_height*3), font, font_size,(255,255,255),font_weight,cv2.LINE_AA)
+        cv2.putText(im2,'HR: 72 BPM',(x+w+2, y+line_height*4), font, font_size,(255,255,255),font_weight,cv2.LINE_AA)
+        cv2.putText(im2,'HRV: 1 BPMM',(x+w+2, y+line_height*5), font, font_size,(255,255,255),font_weight,cv2.LINE_AA)
+        cv2.putText(im2,'D: 2.2m',(x+w+2, y+line_height*6), font, font_size,(255,255,255),font_weight,cv2.LINE_AA)
 
-        cv2.putText(im2,'Risk Assessment: 15.2%, Low Risk',(x+w+2, y+line_height*7), font, font_size+.2,(100,255,100),5,cv2.LINE_AA)
+        cv2.putText(im2,'Risk: 15.2%',(x+w+2, int(y+line_height*7.3)), font, font_size+.2,(100,255,100),font_weight,cv2.LINE_AA)
 
     cnt += 1
     
